e47d3459cc053c44129c049dd7c7ca7876f12984
jo <doselaka7059@gmail.com>
2024-05-22 15:30:22 +0900

    수정

--- oneLabProject/settings.py
+++ oneLabProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m&1gjc)a!0m&t4-skkigdplzw%e&f)5j@ksztwce%#bqaksqs)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '43.203.236.2', 'localhost','onelab.life']
 
 
 # Application definition
